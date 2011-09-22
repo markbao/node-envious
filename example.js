dd2d06e1dd7fade466c80595f1a4474988af4f74
Mark Bao <mark@markbao.com>
2011-09-22 04:00:59 -0400

    added example file

---
+++ example.js
@@ -0,0 +1,20 @@
+var envious = require('envious');
+
+envious.development = 
+{
+    "mysql_hostname": "dev.local",
+    "site_url": "http://127.0.0.1/",
+}
+
+envious.production = 
+{
+    "mysql_hostname": "masterdb.sterlingcooper.com",
+    "site_url": "http://zombo.com/",
+}
+
+envious.default = "development";
+
+var env = envious.apply();
+
+// if you start your app with `NODE_ENV=proudction node app.js`
+console.log(env.site_url); // => http://zombo.com/
