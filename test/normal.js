bb00ce38f13afbdcf1ddfc09c1c829a7ba9e4f9d
Chris Leishman <chris@leishman.org>
2011-12-18 23:52:06 +0100

    Copy missing properties from default environment

--- test/normal.js
+++ test/normal.js
@@ -8,12 +8,20 @@ envious.development =
 {
     "mongodb_hostname": "dev.local",
     "site_url": "http://127.0.0.1/",
+    "session_secret": "ssssshhhhh",
+    "smtp": {
+        "server": "localhost",
+        "email": "noreply@sterlingcooper.com"
+    }
 }
 
 envious.production = 
 {
     "mongodb_hostname": "masterdb.sterlingcooper.com",
     "site_url": "http://zombo.com/",
+    "smtp": {
+        "server": "smtp.sterlingcooper.com",
+    }
 }
 
 envious.default_env = "development";
@@ -30,5 +38,11 @@ assert.equal(env.mongodb_hostname, 'masterdb.sterlingcooper.com');
 
 console.log('test -- variable site_url should not match that of the development set');
 assert.notEqual(env.site_url, 'http://127.0.0.1/');
+
+console.log('test -- variable session_secret should be that of the default set');
+assert.equal(env.session_secret, 'ssssshhhhh');
+
+console.log('test -- variable smtp.email should be that of the default set');
+assert.equal(env.smtp.email, 'noreply@sterlingcooper.com');
 
 console.log('== all tests passed successfully');
